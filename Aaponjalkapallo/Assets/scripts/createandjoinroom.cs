b0802de861b9cf93a43eb0c1ecf203fb0b9ef516
FrostyFeeter <aapo.teikari@outlook.com>
2021-09-16 19:34:36 +0300

    Lobby kesken

---
+++ Aaponjalkapallo/Assets/scripts/createandjoinroom.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class createandjoinroom : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
