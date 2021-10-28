fabcf0487e72974f881dedfa5cbcebcb3f1c5dd7
FrostyFeeter <aapo.teikari@outlook.com>
2021-10-28 19:25:21 +0300

    mouselook valmis

--- Aaponjalkapallo/Assets/scripts/MouseLook.cs
+++ Aaponjalkapallo/Assets/scripts/MouseLook.cs
@@ -7,10 +7,27 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 250f;
     public float minXAngle = -80f;
     public float maxAngle = 90f;
-    Transform playerbody;
-    private float mouseX;
+    Transform playerBody;
+    private float mouseX;   
     private float mouseY;
 
     private float xRotation = 0f;
     public CursorLockMode mode;
+    void Start()
+    {
+        Cursor.lockState = mode;
+        playerBody = gameObject.transform.parent;
+    }
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") *mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") *mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        xRotation = Mathf.Clamp(xRotation, minXAngle, maxAngle);
+    }
+    
 }
